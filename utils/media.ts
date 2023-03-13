e61e2159efd07fae5f11a1c3043ad1e89e9e3d37
Lukasz Bierowiec <lbierowiec@g2a.com>
2023-03-13 12:23:48 +0100

    add media query

---
+++ utils/media.ts
@@ -0,0 +1,9 @@
+const customMediaQuery = (maxWidth: number) => `@media (min-width: ${maxWidth}px)`;
+
+export const media = {
+  custom: customMediaQuery,
+  xs: customMediaQuery(576),
+  md: customMediaQuery(768),
+  sm: customMediaQuery(992),
+  lg: customMediaQuery(1200),
+};
