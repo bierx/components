5c6cae041c61d1f99124e7a89819d9d71f4d5d5d
Lukasz Bierowiec <lbierowiec@g2a.com>
2023-04-18 16:10:36 +0200

    update

--- components/password-checker-widget/password-checker-widget.component.tsx
+++ components/password-checker-widget/password-checker-widget.component.tsx
@@ -1,10 +1,34 @@
 import React, { FunctionComponent } from 'react';
-import { PASSWORD_REQUIREMENTS } from '../../lib/validations';
 import { texts } from './misc/texts';
 import Icon from './atoms/icon.component';
 
 import * as S from './password-checker-widget.style';
 import { IPasswordCheckerWidget } from './password-checker-widget.types';
+
+export const HAS_LEAST_ONE_NUMBER = /(?=.*[0-9])/;
+export const HAS_LEAST_ONE_SPECIAL_CHARACTER = /(?=.*[!@#$%^&*])/;
+
+export const checkLength = (string: string) => string.length >= INPUT_VALIDATION_RULES.minLength;
+
+export const checkCharactersAndDigits = (string?: string) => {
+  if (!string) {
+    return false;
+  }
+
+  return HAS_LEAST_ONE_SPECIAL_CHARACTER.test(string) || HAS_LEAST_ONE_NUMBER.test(string);
+};
+
+export const PASSWORD_REQUIREMENTS = [
+  {
+    name: 'password_check_1' as const,
+    checker: checkLength,
+  },
+  {
+    name: 'password_check_2' as const,
+    checker: checkCharactersAndDigits,
+  },
+];
+
 
 const PasswordCheckerWidget: FunctionComponent<IPasswordCheckerWidget> = ({
   password = '',
